878a86ca6e9564a530ee55c33f3e96f0300f42c3
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-08-19 03:24:56 +0530

    feat(mappings): add explicit shortcut aliases

--- src/mappings/config.ts
+++ src/mappings/config.ts
@@ -3,51 +3,51 @@ export default {
 	'symphonysimper.com/color': ['color', 'c'],
 
 	// Bank
-	'unifiedportal-mem.epfindia.gov.in/memberinterface': ['epfo'],
-	'passbook.epfindia.gov.in/MemberPassBook/login': ['epfo-passbook'],
+	'unifiedportal-mem.epfindia.gov.in/memberinterface': ['epfo', 'pf'],
+	'passbook.epfindia.gov.in/MemberPassBook/login': ['epfo-passbook', 'pf-book'],
 
 	// Bills
-	'login.airfiber.co.in/customer_portal': ['airfiber'],
+	'login.airfiber.co.in/customer_portal': ['airfiber', 'afr'],
 	'pli.indiapost.gov.in': ['indian-post-insurance', 'pli'],
 
 	// Dev
-	'github.com/SymphonySimper/.dotfiles': ['dotfiles', 'nix-config'],
-	'github.com/SymphonySimper': ['github'],
-	'fonts.google.com': ['google-fonts'],
-	'leetcode.com': ['leetcode'],
-	'regex101.com': ['regex101'],
-	'svelte.dev': ['svelte'],
-	'svelte-changelog.vercel.app': ['svelte-changelog'],
-	'tailwindcss.com/docs/installation': ['tailwindcss'],
+	'github.com/SymphonySimper/.dotfiles': ['dotfiles', 'dots', 'nix-config'],
+	'github.com/SymphonySimper': ['github', 'gh'],
+	'fonts.google.com': ['google-fonts', 'gf'],
+	'leetcode.com': ['leetcode', 'lt'],
+	'regex101.com': ['regex101', 're101'],
+	'svelte.dev': ['svelte', 'sv'],
+	'svelte-changelog.vercel.app': ['svelte-changelog', 'sv-log'],
+	'tailwindcss.com/docs/installation': ['tailwindcss', 'tailwind'],
 
 	// Email
-	'mail.google.com/mail/u/0': ['gmail'],
-	'mail.google.com/mail/u/1': ['gmail-1'],
-	'mail.google.com/mail/u/2': ['gmail-2'],
+	'mail.google.com/mail/u/0': ['gmail', 'gm'],
+	'mail.google.com/mail/u/1': ['gmail-1', 'gm1'],
+	'mail.google.com/mail/u/2': ['gmail-2', 'gm2'],
 
 	// Google
-	'drive.google.com': ['google-drive'],
-	'meet.google.com': ['google-meet'],
+	'drive.google.com': ['google-drive', 'drive'],
+	'meet.google.com': ['google-meet', 'meet'],
 
 	// Misc
 	'chromeenterprise.google/policies': ['chrome-enterprise-policy-list'],
 
 	// Nix
-	'nix.dev/manual/nix/latest/language/builtins.html': ['nix-builtins'],
-	'nixpkgs-tracker.ocfox.me': ['nix-pr-tracker'],
-	'wiki.nixos.org/wiki/NixOS_Wiki': ['nix-wiki'],
-	'noogle.dev': ['noogle'],
+	'nix.dev/manual/nix/latest/language/builtins.html': ['nix-builtins', 'nb'],
+	'nixpkgs-tracker.ocfox.me': ['nix-pr-tracker', 'npt'],
+	'wiki.nixos.org/wiki/NixOS_Wiki': ['nix-wiki', 'nw'],
+	'noogle.dev': ['noogle', 'ng'],
 
 	// Social Media
-	'discord.com/channels/@me': ['discord'],
-	'www.reddit.com': ['reddit'],
-	'web.whatsapp.com': ['whatsapp'],
-	'youtube.com': ['youtube'],
+	'discord.com/channels/@me': ['discord', 'dis'],
+	'www.reddit.com': ['reddit', 'rt'],
+	'web.whatsapp.com': ['whatsapp', 'wp'],
+	'youtube.com': ['youtube', 'yt'],
 
 	// Utility
-	'excalidraw.com': ['excalidraw'],
+	'excalidraw.com': ['excalidraw', 'ed', 'draw'],
 	'www.keybr.com': ['keybr'],
-	'monkeytype.com': ['monkeytype'],
-	'squoosh.app': ['squoosh', 'image-compress'],
-	'www.virustotal.com': ['virustotal'],
+	'monkeytype.com': ['monkeytype', 'mt'],
+	'squoosh.app': ['squoosh', 'sq', 'image-compress'],
+	'www.virustotal.com': ['virustotal', 'vt'],
 } satisfies Record<string, Array<string>>;
