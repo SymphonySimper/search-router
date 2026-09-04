5b63c8c542c643f86f903ed29de96d444b3c3f9f
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-09-04 11:26:27 +0530

    config: add mdn playground

--- src/config.ts
+++ src/config.ts
@@ -37,7 +37,7 @@ export const CONFIG: Record<string, Array<Target>> = {
 
 	// Dev / web
 	'fonts.google.com': [{ keys: ['google-fonts', 'gf'] }],
-	'developer.mozilla.org': [{ keys: ['mdn'] }],
+	'developer.mozilla.org': [{ keys: ['mdn'] }, { keys: ['html', 'css', 'js'], path: '/en-US/play' }],
 	'svelte.dev': [{ keys: ['svelte', 'sv'] }],
 	'svelte-changelog.dev': [{ keys: ['svelte-changelog', 'sv-log'] }],
 	'tailwindcss.com': [{ keys: ['tailwindcss', 'tailwind'], path: '/docs/installation' }],
